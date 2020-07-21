2cc57c766cb95fe794a799e528b6fd6e39282c1b
bhawan0407 <bhawandtu93@gmail.com>
2020-07-21 13:10:50 +0530

    Added even valued grandparent solution using simple recursive DFS

---
+++ Medium/sum-of-nodes-with-even-valued-grandparent.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int func(TreeNode* root)
+    {
+        if(root == NULL)
+        {
+            return 0;
+        }
+        
+        int ans = 0;
+        
+        if((root->val) % 2 == 0)
+        {
+            if(root->left && root->left->left)
+            {
+                ans += root->left->left->val;
+            }
+            
+            if(root->left && root->left->right)
+            {
+                ans += root->left->right->val;
+            }
+            
+            if(root->right && root->right->left)
+            {
+                ans += root->right->left->val;
+            }
+            
+            if(root->right && root->right->right)
+            {
+                ans += root->right->right->val;
+            }
+        }
+        
+        return ans + func(root->left) + func(root->right);
+        
+    }
+    
+    
+    int sumEvenGrandparent(TreeNode* root) {
+        
+        return func(root);
+        
+    }
+};
