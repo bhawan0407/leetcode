96c5e4faf9be77938b7b60c662fedf3e49e66359
bhawan0407 <bhawandtu93@gmail.com>
2020-08-04 15:29:12 +0530

    Added power of four O(1) solution

---
+++ Easy/power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        
+        if(num <= 0)
+        {
+            return false;
+        }
+            
+        return ((num&(num-1)) == 0) && !(num & 0xAAAAAAAA); 
+    }
+};
